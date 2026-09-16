34cc282bad8dbd8639ed0b1e84c6b27540f16d1f
Tymur Rezaei <rezai.timur.04@gmail.com>
2026-09-15 21:28:13 -0700

    merged from another repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,88 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+  return ( 
+    <div class = "body">
+      <div class = "void"></div>
+      <div class = "article_wrapper">
+        <article class = "article_header">
+          <p class = "article_title">
+            Pottery for palestine (this will behandwritten?)
           </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+          <div class = "p_wrapper">
+            <p class = "p1">
+              100% of each item’s price goes to the World Food Program’s Palestine fund, which has been declared an emergency. 
+            </p>
+            <p class = "p2">
+              Each $100 donated provides around 135 emergency meals, focused primarily in Gaza but also supporting the West Bank.
+            </p>
+            <p class = "p3">
+              Proof of donation will be uploaded on this website and posted on @ayla_drawss on instagram.
+            </p>
+            <p class = "p4">
+              For more information or to donate yourself, visit&nbsp; 
+              <a href="https://www.wfp.org/emergencies/palestine-emergency">WFP’sPalestine page.</a>
+            </p>
+          </div>
+        </article> 
+        <article class = "article_body">
+          <p class = "body_header_green">
+            Inspired by Palestinian Tatreez, a traditional embroidery style 
 
-      <div className="ticks"></div>
+          </p>
+          <p class = "body_header">            Learn more at&nbsp;
+            <a href="https://www.tatreezandtea.com/">tatreezandtea</a>
+              ,&nbsp; 
+              <a href="https://tatreeztraditions.com/">tatreeztraditions</a>
+          </p>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          <div class = "body_total_wrapper">
+            <div class = "cubes_wrapper">
+              <div class = "top_cube"></div>
+              <div class = "bot_cube"></div>
+            </div>
+            <article class = "body_wrapper">
+            <p class = "body_title body_header_green">Coffee Bean Motif</p>
+            <p class = "body_article">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, ea.
+            </p>
+            </article>
+          </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          <div class = "body_total_wrapper">
+            <article class = "body_wrapper">
+            <p class = "body_title body_header_green">Damascus Rose Motif</p>
+            <p class = "body_article">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, ea.
+            </p>
+            </article>
+             <div class = "cubes_wrapper">
+              <div class = "top_cube"></div>
+              <div class = "bot_cube"></div>
+            </div>
+         </div>
+
+
+          <div class = "body_total_wrapper">
+            <div class = "cubes_wrapper">
+              <div class = "top_cube"></div>
+              <div class = "bot_cube"></div>
+            </div>
+            <article class = "body_wrapper">
+            <p class = "body_title body_header_green">Other Motif</p>
+            <p class = "body_article">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, ea.
+            </p>
+            </article>
+          </div>
+
+        </article>
+      </div>
+      <div class = "void"></div>
+    </div>
   )
 }
 
